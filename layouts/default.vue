5da5fb74f330d3c4dc05ef80946762e6cfe6a233
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-06-30 11:59:20 +0200

    :fire: Remove old stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,16 +7,8 @@ const localePath = useLocalePath();
 
 <template>
   <div>
-    <header class="w-full flex justify-between">
-      <div class="flex gap-3 items-center">
-        <NuxtLink :to="localePath('/')">{{ $t('pages.home') }}</NuxtLink>
-        <NuxtLink :to="localePath('/about')">{{ $t('pages.about') }}</NuxtLink>
-      </div>
+    <header>
       <ThemeSelector />
-      <div class="flex gap-3 items-center">
-        <NuxtLink :to="switchLocalePath('fr')">Français</NuxtLink>
-        <NuxtLink :to="switchLocalePath('en')">English</NuxtLink>
-      </div>
     </header>
 
     <slot />
