913f309816b5b7e203d561b5d80e052d011deba8
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-06-30 11:59:39 +0200

    :rocket: Create the language selector component

---
+++ components/globals/LangSelector.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {useSwitchLocalePath} from "#i18n";
+import {Button} from "~/components/ui/button";
+
+const switchLocale = useSwitchLocalePath();
+</script>
+
+<template>
+  <div class="flex">
+    <Button variant="link" class="p-3" as-child>
+      <NuxtLink :to="switchLocale('fr')" data-lang-link active-class="active">FR</NuxtLink>
+    </Button>
+    <Button variant="link" class="p-3" as-child>
+      <NuxtLink :to="switchLocale('en')" data-lang-link active-class="active">EN</NuxtLink>
+    </Button>
+  </div>
+</template>
+
+<style scoped lang="sass">
+[data-lang-link]
+  &.active
+    @apply underline
+</style>
