0d771c22e472154c22a701c14f2daa4200645fd1
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-01 12:37:20 +0200

    :rocket: Setup toast toasts and terms and conditions checkbox

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -6,12 +6,14 @@ import {Input} from "~/components/ui/input";
 import {FormField, FormItem, FormControl, FormLabel} from "~/components/ui/form";
 import {passwordRegExp, userRegExp} from "~/utils/regexp.utils";
 import type {GenericObject} from "vee-validate";
+import {useToast} from "~/components/ui/toast";
 
 definePageMeta({
   layout: 'auth',
 });
 
 const localePath = useLocalePath();
+const { t } = useI18n();
 
 const schema = toTypedSchema(z.object({
   username: z.string().min(1).regex(userRegExp),
@@ -19,22 +21,26 @@ const schema = toTypedSchema(z.object({
   email: z.string().email().min(1),
   password: z.string().min(1).regex(passwordRegExp),
 }));
+const { toast } = useToast();
+
+const termsAndConditions = ref<boolean>(false);
 
 const handleSubmit = async ({ username, displayName, email, password }: GenericObject) => {
-  const pass = [];
-  for (let i = 0; i < password.length; ++i) pass.push('·');
-
+  toast({
+    title: t("pages.auth.register.toasts.userCreated.title"),
+    description: t("pages.auth.register.toasts.userCreated.description")
+  });
   console.table({
     username,
     displayName,
     email,
-    password: pass.join(''),
+    password: password.split('').map((c: string) => '·').join(''),
   });
 };
 </script>
 
 <template>
-  <AuthCard :schema="schema" @submit="handleSubmit">
+  <AuthCard :schema="schema" @submit="handleSubmit" :submit-disabled="!termsAndConditions">
     <template #title>{{ $t('pages.auth.register.title') }}</template>
     <template #formContent>
       <FormField name="username" v-slot="{ componentField }">
@@ -75,6 +81,14 @@ const handleSubmit = async ({ username, displayName, email, password }: GenericO
                 :placeholder="$t('pages.auth.register.inputs.password.placeholder')"
             />
           </FormControl>
+        </FormItem>
+      </FormField>
+      <FormField name="termsAndConditions" v-slot="{ componentField }">
+        <FormItem class="md:col-span-2 flex items-end gap-3 mt-4">
+          <FormControl>
+            <Checkbox v-bind="componentField" :checked="termsAndConditions" @update:checked="termsAndConditions = $event" />
+          </FormControl>
+          <FormLabel>{{ $t("pages.auth.register.inputs.termsAndConditions") }}</FormLabel>
         </FormItem>
       </FormField>
     </template>
