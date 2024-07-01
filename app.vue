a0f82578c5bdf80530e663addb3e868753d463cf
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-01 12:36:23 +0200

    :hammer: Configure the Toaster component imported from shadcn

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import {Toaster} from "~/components/ui/toast";
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
+
+    <Toaster />
 
     <NuxtLayout>
       <NuxtPage />
