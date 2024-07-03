0ab92419546c46842834737d1fd79a1373265deb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-04 00:11:41 +0200

    :lipstick: Update style to match with mobile devices

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -3,8 +3,8 @@ import LangSelector from "~/components/globals/LangSelector.vue";
 </script>
 
 <template>
-  <div data-layout="auth" class="relative w-full min-h-dvh grid place-items-center">
-    <div class="fixed top-0 left-1/2 -translate-x-1/2 py-3"><LangSelector /></div>
+  <div data-layout="auth" class="relative w-full min-h-dvh flex flex-col justify-start items-center lg:justify-center">
+    <div class="py-3 lg:fixed lg:top-0 lg:left-1/2 lg:-translate-x-1/2"><LangSelector /></div>
     <slot />
   </div>
 </template>
