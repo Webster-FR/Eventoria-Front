b44e2e7da549d4f781164c5d09b7bc8bd7a3cf4c
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-04 00:13:34 +0200

    :rocket: Add a PreferenceCard component

---
+++ components/specifics/preferences/PreferenceCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <Card class="flex-1">
+    <CardHeader>
+      <CardTitle><slot name="title" /></CardTitle>
+    </CardHeader>
+    <Separator />
+    <CardContent class="p-6">
+      <slot />
+    </CardContent>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
