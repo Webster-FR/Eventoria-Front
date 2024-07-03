cc3522e5215668ec33d6b70e76a97aae9d08cb74
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-03 14:18:42 +0200

    :rocket: Add a user provider component to handle fallback content when not logged in

---
+++ components/specifics/user/UserProvider.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const user = useState('user').value;
+</script>
+
+<template>
+  <div v-if="user">
+    <slot name="default" />
+  </div>
+  <div v-else>
+    <slot name="fallback" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
