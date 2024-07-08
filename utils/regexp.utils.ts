b538ad494b723e6c2228fb9da37f309236e79a88
loicmaes <production@maesloic.fr>
2024-07-08 13:31:33 +0200

    :hammer: Update display name regex to accept spaces

--- utils/regexp.utils.ts
+++ utils/regexp.utils.ts
@@ -1,3 +1,3 @@
 export const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_&é"'(§è!çà)\-°?,.;/:+=\\^¨$`£ù%µπÉÈÀÇâêôûòãñõ@#]).{8,}$/;
 export const usernameRegExp = /^(?=.*[a-z])[a-z._]{8,24}$/;
-export const displayNameRegExp = /^[\w.\-ïîôöõñêëéèàäâãçÉÈÀÇÙùÏÎÕÑÔÖ]{2,32}$/;
+export const displayNameRegExp = /^[\w.\- ïîôöõñêëéèàäâãçÉÈÀÇÙùÏÎÕÑÔÖ]{2,32}$/;
